d7644f212dc87bdfa523e40ec93b2d7eeb529a6f
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-11-23 11:13:11 +0600

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,20 +4,16 @@ import {
 
 // Auto generates routes from vue files under ./pages
 // https://vitejs.dev/guide/features.html#glob-import
-const pages = import.meta.glob('./pages/*.vue')
-
-const arr = [];
+const pages = import.meta.glob('./pages/*.vue') 
 
 const routes = Object.keys(pages).map((path) => {
-  const name = path.match(/\.\/pages(.*)\.vue$/)[1].toLowerCase();
-  arr.push(  name.includes('_') ? name: '' );
+  const name = path.match(/\.\/pages(.*)\.vue$/)[1].toLowerCase(); 
   return {
     path: name === '/home' ? '/' : name,
     component: pages[path] // () => import('./pages/*.vue')
   }
 })
-
-// console.log(arr.includes('/_test') );
+ 
 
 export function createRouter() {
   return _createRouter({
